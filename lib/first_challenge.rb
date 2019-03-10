e9d6605a9f18e2713f5950881966dea28ffce098
Eric Storey <estorey@gmail.com>
2019-03-10 22:49:15 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,18 +16,18 @@ def first_challenge
   }
 
   #your code here
-contacts.each {|person, data|
-  if person=="Freddy Mercury"
-    data.each {|attribute, value|
-      if value== :favorite_icecream_flavors
-        value.delete_if { |flavor|
-          flavor=="strawberry"
-          binding.pry
-        }
-      end
-    }
-  end
-}
+  contacts.each {|person, data|
+    if person=="Freddy Mercury"
+      data.each {|attribute, value|
+        if value== :favorite_icecream_flavors
+          value.delete_if { |flavor|
+            flavor=="strawberry"
+            binding.pry
+          }
+        end
+      }
+    end
+  }
 
   #remember to return your newly altered contacts hash!
   contacts
